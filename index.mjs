b5ffc6369e579723fe3b385dca8855ef5008692c
unknown <boogerlad@gmail.com>
2023-07-29 01:45:38 -0400

    yay

--- index.mjs
+++ index.mjs
@@ -1,62 +1,71 @@
 import 'dotenv/config';
-import { Client, Events, GatewayIntentBits } from 'discord.js';
+import { Client, Events, GatewayIntentBits, AttachmentBuilder, EmbedBuilder } from 'discord.js';
 
 const client = new Client({
 	intents: [GatewayIntentBits.Guilds],
 });
-
+let channel;
 client.once(Events.ClientReady, c => {
 	console.log(`Ready! Logged in as ${c.user.tag}`);
+	channel = client.channels.cache.get(process.env.DISCORD_CHANNEL_ID);
 });
 
-import * as https from 'https';
+import gm from 'gm';
+const im = gm.subClass({ imageMagick: true });
 
-function req(url, method, headers, body) {
-	return new Promise((resolve, reject) => {
-		let req = https.request(url, {headers, method}, resp => {
-			if(resp.statusCode !== 200) {
-				reject({"statusCode" : resp.statusCode});
-			}
-			let data = '';
-			resp.on('data', chunk => data += chunk);
-			resp.on('end', () => resolve(data));
-		}).on("error", reject);
-		if(body) {
-			req.write(body);
-		}
-		req.end();
-	});
+import { chromium } from 'playwright';
+const browser = await chromium.launch({headless: false});
+const page = await browser.newPage();
+page.setViewportSize({ width: 2000, height: 2000 })
+async function login() {
+	await page.goto("https://trade-alert.com/jbot/launch/");
+	await page.locator("#username").fill(process.env.TRADE_ALERT_USERNAME);
+	await page.locator("#password").fill(process.env.TRADE_ALERT_PASSWORD);
+	await page.locator('input[value="Login"]').click();
 }
-
-import { spawn } from 'node:child_process';
-
-client.on(Events.InteractionCreate, async interaction => {
-	if(interaction.isChatInputCommand() && interaction.commandName === 'moso') {
-		let data = "Failed to reach trade-alert.com API";
-		try {
-			let externalresolve;
-			let p = new Promise((resolve, reject) => {
-				externalresolve = resolve;
-			});
-			data = (await req(`https://quant.trade-alert.com/?cmd=moso&apikey=${process.env.TRADE_ALERT_API_KEY}&symbol=${interaction.options.getString('ticker')}`));
-			const column = spawn('column', ['-t']);
-			let data2 = [];
-			column.stdout.on('data', d => data2.push(d.toString()));
-			column.stdin.write(data);
-			column.stdin.end();
-			column.on('close', async code => {
-				if(code === 0) {
-					await interaction.reply('```' + data2.join('\n').slice(0, 1900) + '```');
-					externalresolve();
-				} else {
-					console.log(`grep process exited with code ${code}`);
+await login();
+page.on('response', async response => {
+	console.log('<<', response.status(), response.url());
+	if(response.url() === 'https://trade-alert.com/http-bind') {
+		let t = await response.text()
+		console.log(t);
+		if(t.includes('terminate')) {
+			await login();
+		} else {
+			let r = await page.evaluate(() => {
+				let n = document.querySelector('.incoming:last-of-type .ta-small');
+				if(n && window.x !== n) {
+					window.x = n;
+					if(n.querySelector('table')) {
+						return 'picture';
+					} else {
+						return n.innerText;
+					}
 				}
 			});
-			return p;
-		} catch(e) {
-			console.error(interaction);
-			console.error(e);
+			if(r) {
+				if(r === 'picture') {
+					im(await page.locator('.incoming:last-of-type .ta-small').screenshot())
+					.trim()
+					.toBuffer('PNG', function(err, buffer) {
+						if(err) {
+							return console.error(err);
+						}
+						channel.send({files: [new AttachmentBuilder(buffer)]});
+					});
+				} else {
+					channel.send(r);
+				}
+			}
 		}
+	}
+});
+
+client.on(Events.InteractionCreate, async interaction => {
+	if(interaction.isChatInputCommand() && interaction.commandName === 'ta') {
+		await page.locator('#msgArea').fill(interaction.options.getString('command'));
+		await page.locator('#send-command-btn').click();
+		await interaction.reply('sent command to terminal');
 	}
 });
 
--- register.mjs
+++ register.mjs
@@ -5,14 +5,15 @@ const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_BOT_JWT);
 
 try {
 	console.log('Started refreshing application (/) commands.');
-	await rest.put(Routes.applicationCommands(process.env.DISCORD_APPLICATION_ID), {body: [
+	await rest.put(Routes.applicationCommands(process.env.DISCORD_APPLICATION_ID), { body: [] });
+	await rest.put(Routes.applicationGuildCommands(process.env.DISCORD_APPLICATION_ID, process.env.DISCORD_GUILD_ID), {body: [
 		{
-			name: 'moso',
-			description: 'trade-alert.com most active stock options',
+			name: 'ta',
+			description: 'https://trade-alert.com/jbot/launch/',
 			options: [
 				{
-					name: 'ticker',
-					description: 'for example: AAPL, AMD, aapl, amd',
+					name: 'command',
+					description: 'for example: /moso amd, /oichg tsla, aapl, /oichg @top50, /top nvda where buyer and not complex',
 					type: 3, // Type 3 represents a string.
 					required: true,
 				},
